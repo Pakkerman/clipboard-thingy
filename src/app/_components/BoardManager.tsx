dd966f2dc79dd11a0f6349199798a0df568ef021
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-11-09 17:10:50 +0800

    FIX: Issue with loading board id from localstrage before the BoardManager is renders, move getting from localstorage into useEffect and loading to board id input

--- src/app/_components/BoardManager.tsx
+++ src/app/_components/BoardManager.tsx
@@ -4,25 +4,27 @@ import React, { useEffect, useState } from "react"
 import Link from "next/link"
 
 // getting board from localstorage, if there's no board, generate new
-const localStorageId = localStorage.getItem("clipbroker_boardId")
-console.log(typeof localStorageId)
-if (!localStorageId || localStorageId === "undefined") {
-  const newId = generateNewBoardId()
-  localStorage.setItem("clipbroker_boardId", newId)
-}
-const boardId = localStorage.getItem("clipbroker_boardId")!
 
 export default function BoardManager() {
-  const [inputId, setInputId] = useState(boardId)
+  const [inputId, setInputId] = useState("Loading...")
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    const localStorageId = localStorage.getItem("clipbroker_boardId")
+    if (!localStorageId || localStorageId === "undefined") {
+      const newId = generateNewBoardId()
+      localStorage.setItem("clipbroker_boardId", newId)
+    }
+    const boardId = localStorage.getItem("clipbroker_boardId")!
+
     setInputId(boardId)
+    setLoading(false)
   }, [])
 
   return (
     <section className="flex flex-col gap-4 py-6 font-chakraPetch">
       <Link
-        className="rounded-xl border border-black/20 border-orange-400 bg-orange-400 p-2 text-lg text-orange-950 shadow-md shadow-orange-950/40 transition hover:shadow-inner hover:shadow-orange-950 active:shadow-inner active:shadow-orange-950"
+        className="rounded-xl border border-orange-400 bg-orange-400 p-2 text-lg text-orange-950 shadow-md shadow-orange-950/40 transition hover:border-orange-500 hover:bg-orange-500 hover:shadow-none hover:shadow-orange-950 active:shadow-inner active:shadow-orange-950"
         href={"/" + inputId}
       >
         <div className="text-center">Start</div>
