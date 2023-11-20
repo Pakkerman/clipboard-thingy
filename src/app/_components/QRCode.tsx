e4e6bdba41b76bd70317895f36b19e06a4290dc3
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-11-20 16:09:40 +0800

    remove QRCode url text display

--- src/app/_components/QRCode.tsx
+++ src/app/_components/QRCode.tsx
@@ -14,12 +14,15 @@ export default function QRCode() {
   if (url === "") return <></>
   return (
     <>
-      <p className="p-2">Scan to open this board on other devices</p>
+      <br />
       <QRCodeSVG
         value={url}
         className="rounded-xl border border-orange-800/20 p-2 shadow-inner shadow-orange-950/20"
       />
-      <p>{url}</p>
+      <p className="p-2 text-slate-700">
+        Scan to open this board on other devices
+      </p>
+      {/* <p>{url}</p> */}
     </>
   )
 }
