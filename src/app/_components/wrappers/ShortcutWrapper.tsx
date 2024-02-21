554c9aedfefffe28542ec718a67bb83b1850aca6
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-02-21 13:56:31 +0800

    feat: change short indicator to icons

--- src/app/_components/wrappers/ShortcutWrapper.tsx
+++ src/app/_components/wrappers/ShortcutWrapper.tsx
@@ -1,19 +1,21 @@
 import React from "react"
 import { isMobile } from "react-device-detect"
 
+import { BsArrowReturnLeft } from "react-icons/bs"
+import { ImCommand } from "react-icons/im"
+const ButtonIcon = {
+  cmd: <ImCommand />,
+  enter: <BsArrowReturnLeft />,
+}
+
 // Parent needs to have relative class
 type ShortcutWrapperProps = { shortcuts: Array<string> }
 export default function ShortcutWrapper({ shortcuts }: ShortcutWrapperProps) {
   if (isMobile) return <></>
   return (
-    <ul className="absolute right-3 top-[50%] flex translate-y-[-50%] items-center gap-2 opacity-80">
+    <ul className="absolute right-4 top-[50%] flex h-full translate-y-[-50%] items-center justify-center gap-2 opacity-70">
       {shortcuts.map((item, idx) => (
-        <div
-          key={idx}
-          className="rounded-md border-[0.5px] border-gray-800 px-2 py-1 text-xs text-gray-800"
-        >
-          {item}
-        </div>
+        <div key={idx}>{ButtonIcon[item]}</div>
       ))}
     </ul>
   )
